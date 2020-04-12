918044782984cdce5e84634fda7295a54844d8e0
taiki.mikami <mik036ta@gmail.com>
2020-04-12 08:04:24 +0000

    ranking

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -2,7 +2,7 @@ class RankingController < ApplicationController
     before_action :require_user_logged_in
     
     def index
-        @user = User.all
-        @study_records = StudyRecord.all
+        @users = User.all
+        @ranking = StudyRecord.select("study_records.user_id,sum(study_records.studytime) as study_sum").group("study_records.user_id").order("study_sum desc")
     end
 end
